4abc8ff24c0885dfe68f4c14ccb09deaa5c625ee
Suhas Chitade <suhaschitade@Suhass-MacBook-Air-2.local>
2018-10-02 23:43:32 +0530

    First blog commit

---
+++ src/app/shared/services/article.model.ts
@@ -0,0 +1,9 @@
+export class Article {
+    title:string,
+    short_descr:string,
+    descr:string,
+    active:boolean,
+    articleImage:string,
+    created_at:string
+}
+
---
+++ src/app/shared/services/article.service.ts
@@ -0,0 +1,16 @@
+import { Injectable} from '@angular/core'
+import { Response, Http, Headers, HttpModule } from '@angular/http'
+import { Observable } from 'rxjs'
+import { Article } from './article.model'
+
+import { Title } from '@angular/platform-browser'
+
+let articleURL = "http://localhost:8000/articles"
+
+@Injectable()
+export class ArticleService{
+
+    let 
+}
+
+
